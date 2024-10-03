ada4ab453e2b56bb1effb9ee64a62840bf2d4e68
AndriiZhovtko <andriy.zhovtko@gmail.com>
2024-10-03 12:05:46 +0200

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,9 @@
+"use strict";
+const wall = document.querySelector(".wall");
+const spider = document.querySelector(".spider");
+const centerWidth = (wall.clientWidth - spider.clientWidth) / 2;
+const centerHeight = (wall.clientHeight - spider.clientHeight) / 2;
+spider.style.left = centerWidth + "px";
+spider.style.top = centerHeight + "px";
+
+//# sourceMappingURL=index.f75de5e1.js.map
